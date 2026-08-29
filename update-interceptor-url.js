fabf936763d466f7878c87a88df7a8286f793a3a
Naga <efobifrancis53@gmail.com>
2026-08-29 03:10:14 +0100

    chore: cleanup scripts

--- update-interceptor-url.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require("fs");
-let content = fs.readFileSync("frontend/src/components/fetch-interceptor.tsx", "utf8");
-content = content.replace(
-  "let [resource, config] = args;", 
-  "let [resource, config] = args;\n        if (typeof resource === 'string' && resource.includes('inkto.jointaccount.org/api/')) {\n          const url = new URL(resource);\n          url.searchParams.set('_t', Date.now().toString());\n          resource = url.toString();\n        }"
-);
-fs.writeFileSync("frontend/src/components/fetch-interceptor.tsx", content);
-console.log("Added cache buster to URL");

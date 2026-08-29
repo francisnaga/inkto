598ebe3d582faf9f350d2cfb3b796c044ff66aa6
Naga <efobifrancis53@gmail.com>
2026-08-29 02:15:35 +0100

    chore: remove script files

--- update-nav-flex.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require("fs");
-let content = fs.readFileSync("frontend/src/components/app-layout.tsx", "utf8");
-content = content.replace(/<div className="flex justify-between items-center h-14 px-4">/, '<div className="flex justify-around items-center h-14">');
-fs.writeFileSync("frontend/src/components/app-layout.tsx", content);
-console.log("Updated to justify-around");

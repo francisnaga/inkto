00ba5b5ca3674b849fb5601a586088ba56742ade
Naga <efobifrancis53@gmail.com>
2026-08-29 02:58:25 +0100

    chore: remove script files

--- update-interceptor.js
+++
@@ -1,5 +0,0 @@
-﻿const fs = require("fs");
-let content = fs.readFileSync("frontend/src/components/fetch-interceptor.tsx", "utf8");
-content = content.replace("config.headers = config.headers || {};", "config.headers = config.headers || {};\n          config.credentials = 'include';");
-fs.writeFileSync("frontend/src/components/fetch-interceptor.tsx", content);
-console.log("Added credentials: include");

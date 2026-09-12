b87f179d59376e0a08cd29a07fa754f2e0e0c8b4
Naga <efobifrancis53@gmail.com>
2026-09-12 03:18:46 +0100

    fix: entirely remove all capacitor config files from repo to finally fix vercel build

--- frontend/capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'com.inkto.app',
-  appName: 'Inkto',
-  webDir: 'out',
-  plugins: { CapacitorHttp: { enabled: true } }
-};
-export default config;
